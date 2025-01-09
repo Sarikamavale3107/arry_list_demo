67b01f4978a001e8d9e8d1514e3e57a1eb5c0dbf
Sarika Mavale <sarikamavale3107@gmail.com>
2025-01-09 20:32:37 +0530

    ArrayList code added for city

---
+++ NovJavaBatchDemo/src/com/velocity/ArrayListDemo1.java
@@ -0,0 +1,17 @@
+package com.velocity;
+
+import java.util.ArrayList;
+
+public class ArrayListDemo1 {
+	
+    public static void main(String[]args) {
+    	
+    	ArrayList<String> al = new ArrayList<String>();
+    	al.add("Mumbai");
+    	al.add("Pune");
+    	al.add("Bangalore");
+    	al.add("Nashik");
+    	System.out.println(al);
+    }
+
+}
